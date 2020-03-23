2346b701fd4bedbdb64c0cee09c8745ce0df2936
june.park <june@ubiquix.com>
2020-03-23 11:49:22 +0900

    uhd

--- logic_normal.py
+++ logic_normal.py
@@ -174,7 +174,7 @@ class LogicNormal(object):
             else:
                 logger.debug('cml - movie ' + item['name'])
                 rules = ['4K', '4k', 'UHD', '2160p', '2160P']
-                for keywords in rules.key():
+                for keywords in rules:
                     #gregx = re.compile(keywords, re.I)
                     if (gregx.search(item['name'])) is not None:
                         logger.debug('cml - uhd condition match %s : %s', keywords, item['name'])
