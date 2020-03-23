909f0f9f197e0349830a68009bfd7af5ec276b28
june.park <june@ubiquix.com>
2020-03-23 20:18:11 +0900

    .

--- logic_normal.py
+++ logic_normal.py
@@ -346,11 +346,11 @@ class LogicNormal(object):
             logger.debug('mm - fullPath: %s', data['fullPath'])
             if not os.path.isdir(dest_folder_path):
                 os.makedirs(dest_folder_path)
-            fileCheck = os.path.join(base_path.strip(), set_cat.encode('utf-8'), set_country.encode('utf-8'), set_year.encode('utf-8'), data['dest_folder_name'], data['name'])
-            logger.debug('mm - fileCheck: %s', fileCheck)
-            if not os.path.isfile(fileCheck):
-                os.rename(data['fullPath'], fileCheck)
-                LogicNormal.db_save(data, dest_folder_path)
+                fileCheck = os.path.join(base_path.strip(), set_cat.encode('utf-8'), set_country.encode('utf-8'), set_year.encode('utf-8'), data['dest_folder_name'], data['name'])
+                logger.debug('mm - fileCheck: %s', fileCheck)
+                if not os.path.isfile(fileCheck):
+                    os.rename(data['fullPath'], fileCheck)
+                    LogicNormal.db_save(data, dest_folder_path)
             #fileCheck = os.path.join(base_path.strip(), set_cat.encode('utf-8'), set_country.encode('utf-8'), set_year.encode('utf-8'), data['name'])
             #if not os.path.isfile(fileCheck):
                 #shutil.move(data['fullPath'], dest_folder_path)
