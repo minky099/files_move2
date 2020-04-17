7e0ceb5ea8cfbb1e6556426370e20932f639c972
june.park <june@ubiquix.com>
2020-04-18 01:44:30 +0900

    .

--- api_daum_movie.py
+++ api_daum_movie.py
@@ -263,7 +263,7 @@ class MovieSearch(object):
         try:
             for idx in range(len(movie_list)):
                 logger.debug('smw - score:%s, myear:%s, [%s]year:%s', movie_list[idx]['score'], movie_year, idx, movie_list[idx]['year'])
-                if movie_list[idx]['score'] >= 85 and abs(movie_year - int(movie_list[idx]['year'])) <= 1:
+                if movie_list[idx]['score'] >= 10 and abs(movie_year - int(movie_list[idx]['year'])) <= 1:
                     logger.debug('smw - id(%s):%s', movie_list[idx]['score'], movie_list[idx]['id'])
                     more_url = 'http://movie.daum.net/data/movie/movie_info/detail.json?movieId=%s' % movie_list[idx]['id']
                     meta_data = get_json(more_url)
