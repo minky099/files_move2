ab6eeb58cacc3f7072efb742e0ab13c2af68ab59
june.park <june@ubiquix.com>
2020-04-19 02:33:01 +0900

    .

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from plugin import blueprint, menu, plugin_load, plugin_unload, plugin_info
 
-cookies = {'TIARA': 'UGW1xtn4YKAmqYXfc_FW.vIqTlqAQ1DPsaWrixwHrVf6BsR..W3Yfm2_fJN7Tr97RepQmpIDDP255dKZNCtRRwYq_LnCkF3G'}
-cookies += {'UUID': 'I41mWZivIqIc2.gQmLm2E_TLoaDsof1zYyFdoLTC_hU0'}
-cookies += {'RUID': 'VPav-azRrrcw.q9f5ohG2DG36dxksb7ez6PZomVVMFU0'}
-cookies += {'TUID': 'r5mrQF4b5UFo_200215225759853'}
-cookies += {'XUID': 'AGRX5MKvvwl2h.K.-jQIXcI5dRCc-XSeSmWxEdggU9X_ft3HJWDn2Ji3BHnFVlrK2-l_fUikj6LNMcjXt6kFDw00'}
+cookies = {'TIARA': 'UGW1xtn4YKAmqYXfc_FW.vIqTlqAQ1DPsaWrixwHrVf6BsR..W3Yfm2_fJN7Tr97RepQmpIDDP255dKZNCtRRwYq_LnCkF3G',
+ 'UUID': 'I41mWZivIqIc2.gQmLm2E_TLoaDsof1zYyFdoLTC_hU0',
+ 'RUID': 'VPav-azRrrcw.q9f5ohG2DG36dxksb7ez6PZomVVMFU0',
+ 'TUID': 'r5mrQF4b5UFo_200215225759853'
+ 'XUID': 'AGRX5MKvvwl2h.K.-jQIXcI5dRCc-XSeSmWxEdggU9X_ft3HJWDn2Ji3BHnFVlrK2-l_fUikj6LNMcjXt6kFDw00'}
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
