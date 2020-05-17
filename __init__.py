bcce40e27c9e1e4f9eebf3d2cde73a3ec46ae7b7
june.park <june@ubiquix.com>
2020-05-17 17:58:19 +0900

    updata cookie

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from plugin import blueprint, menu, plugin_load, plugin_unload, plugin_info
 
-cookies = {'TIARA': '8k5QkOh.9GhJ3TpDeOmCtNuRrUECoXjg.l-GGwYVkUM.qSvL28FRKoctiEh-IEF4nfjIm9bZ.ttnZU-4yH5jp2i8FZ3kkYDq',
+cookies = {'TIARA': 'VcmY_PCvkRysqP1NNCp-5tJG8WrssFrWbKT5x5_tTZ8BNbQnv2R.nLTMXhV363YZ1vCOGQrVtWWcdeo9G3WT53A3AR-LXH51',
  'UUID': 'I41mWZivIqIc2.gQmLm2E_TLoaDsof1zYyFdoLTC_hU0',
  'RUID': 'VPav-azRrrcw.q9f5ohG2DG36dxksb7ez6PZomVVMFU0',
  'TUID': 'r5mrQF4b5UFo_200215225759853',
- 'XUID': 'TNk_w39Zf1iZT-2CtZZniKaM9LB1gXPx.KzpQ5WZmsTQYriow2c7jk7haTQEjbpI7Usl2i4l4xhQQQ5F2pHDzQ00'}
+ 'XUID': '5maH8Mqmls9zFJn8TYn9GiIGzgiF9SVMQUttBwO3nsg.ZuGG9t4am.iYhfwQdTq-Bq_vg9QOa.T1xqt1I_EOGw00'}
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
