5ed6c958686d44ee8e41540af94c6ab636fafa21
arkx82 <arkx82@gmail.com>
2021-05-02 18:24:49 +0900

    for sjva3 #6

--- __init__.py
+++ __init__.py
@@ -1,7 +1,18 @@
+try:
+    from guessit import guessit
+except:
+    try:
+        os.system("{} install guessit".format(app.config['config']['pip']))
+        from guessit import guessit
+    except:
+        pass
+
+from .plugin import blueprint, menu, plugin_load, plugin_unload
+
 #from .plugin import blueprint, menu, plugin_load, plugin_unload
-from .plugin import P
-blueprint = P.blueprint
-menu = P.menu
-plugin_load = P.logic.plugin_load
-plugin_unload = P.logic.plugin_unload
-plugin_info = P.plugin_info
+#from .plugin import P
+#blueprint = P.blueprint
+#menu = P.menu
+#plugin_load = P.logic.plugin_load
+#plugin_unload = P.logic.plugin_unload
+#plugin_info = P.plugin_info
