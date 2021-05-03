261526d7ed09cc722bf8add3b2572ceb27ad12da
arkx82 <arkx82@gmail.com>
2021-05-04 00:18:38 +0900

    isHangul update #1

--- logic_normal.py
+++ logic_normal.py
@@ -963,10 +963,17 @@ class LogicNormal(object):
 
     @staticmethod
     def isHangul(text):
-        if type(text) is not py_unicode:
-            encText = text.decode('utf-8')
-        else:
+        #Check the Python Version
+        pyVer3 =  sys.version_info >= (3, 0)
+
+        if pyVer3 : # for Ver 3 or later
             encText = text
+        else: # for Ver 2.x
+            if type(text) is not unicode:
+                encText = text.decode('utf-8')
+            else:
+                encText = text
+
         hanCount = len(re.findall(u'[\u3130-\u318F\uAC00-\uD7A3]+', encText))
         return hanCount > 0
 
